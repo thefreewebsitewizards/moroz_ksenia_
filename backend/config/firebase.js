ba8a3a00f550cd6055223c9758196712f6ff8544
sheenderella <dylan@thefreewebsitewizards.com>
2025-07-31 18:16:19 +0800

    env firebase

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -2,14 +2,18 @@
 
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection } from 'firebase/firestore';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDy5hBMFfqzWQinV0sLLZAFh8koVZE5Qk",
-  authDomain: "ksenia-munoz.firebaseapp.com",
-  projectId: "ksenia-munoz",
-  storageBucket: "ksenia-munoz.appspot.com",
-  messagingSenderId: "982189236395",
-  appId: "1:982189236395:web:594648694ec020153f17ee"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
