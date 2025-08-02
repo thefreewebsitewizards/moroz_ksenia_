4754e8bb034b5dc319ee8b092e72fa1cb3aae4e8
sheenderella <dylan@thefreewebsitewizards.com>
2025-08-02 11:37:59 +0800

    products

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,20 @@
 import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/users.js';
+import productRoutes from './routes/products.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Add a root route
+app.get('/', (req, res) => {
+  res.json({ message: 'Moroz Ksenia Art Backend API is running!' });
+});
+
 // Mount routes
 app.use('/', userRoutes);
+app.use('/api', productRoutes);
 
 app.listen(4000, () => {
   console.log('Server running on http://localhost:4000');
